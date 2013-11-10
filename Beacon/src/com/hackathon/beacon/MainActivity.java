2ffd10c36bb6ba25cdd3bc6147eb12dac0c742f5
stpherring <stpherring@aol.com>
2013-11-09 20:51:54 -0600

    Added Parse functionality

--- Beacon/src/com/hackathon/beacon/MainActivity.java
+++ Beacon/src/com/hackathon/beacon/MainActivity.java
@@ -1,5 +1,7 @@
 package com.hackathon.beacon;
 
+import com.parse.Parse;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -7,13 +9,19 @@ import android.view.Menu;
 public class MainActivity extends Activity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) 
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		String app_id = getString(R.string.parse_app_id);
+		String client_key = getString(R.string.parse_client_key);
+		Parse.initialize(this, app_id, client_key);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(Menu menu) 
+	{
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
